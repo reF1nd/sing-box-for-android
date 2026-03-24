71e69f7d46542427c6f1238faec68f7de3e69835
reF1nd <ref1nd@thunder-cloud.cc>
2026-03-24 21:55:48 +0800

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "io.nekohasekai.sfa"
+        applicationId = "io.reF1nd.sfa"
         minSdk = 24
         targetSdk = 37
         versionCode = getVersionProps("VERSION_CODE").toInt()
